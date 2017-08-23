f26f6514a2b3f65c84c50bae98ae410179c658f6
volitilov <volitilov@gmail.com>
2017-08-23 19:48:07 +0300

    add structure example

---
+++ gulpfile.babel.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+
+import gulp from 'gulp';
+import eslint from 'gulp-eslint';
+import mocha from 'gulp-mocha';
+import flow from 'gulp-flowtype';
+import babel from 'gulp-babel';
+import del from 'del';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
+
+
+const paths = {
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js?(x)',
+  gulpFile: 'gulpfile.babel.js',
+  webpackFile: 'webpack.config.babel.js',
+  clientBundle: 'dist/client-bundle.js?(.map)',
+  allLibTests: 'lib/test/**/*.js',
+  libDir: 'lib',
+  distDir: 'dist',
+};
+
+
+gulp.task('lint', () =>
+  gulp.src([
+    paths.allSrcJs,
+    paths.gulpFile,
+    paths.webpackFile,
+  ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(flow({ abort: true })));
+
+
+gulp.task('clean', () => del([
+  paths.libDir,
+  paths.clientBundle,
+]));
+
+
+gulp.task('test', ['build'], () =>
+  gulp.src(paths.allLibTests)
+    .pipe(mocha()));
+
+
+gulp.task('build', ['lint', 'clean'], () =>
+  gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir)));
+
+
+gulp.task('main', ['test'], () =>
+  gulp.src(paths.clientEntryPoint)
+    .pipe(webpack(webpackConfig))
+    .pipe(gulp.dest(paths.distDir)));
+
+
+gulp.task('watch', () => gulp.watch(paths.allSrcJs, ['main']));
+
+
+gulp.task('default', ['watch', 'main']);
